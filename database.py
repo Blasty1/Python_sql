b9afc55029aa066287ee441cabba7377cae579f6
Blasty1 <40994591+Blasty1@users.noreply.github.com>
2019-04-27 17:54:13 +0200

    Add files via upload

--- database.py
+++ database.py
@@ -1,25 +1,34 @@
 from SQL import connection  #classe per tutte le funzioni database-programam
 import PySimpleGUI as sg #classe per la grafica
 from design import Design_home#classe per la grafica
+import mysql.connector #serve per effettuare operazioni sul db
+import datetime #serve per ottenere il datetime corrente
+
 conn=connection('uswJJmW75Q','bxBA2sMC7s','uswJJmW75Q','remotemysql.com')
-design_home=Design_home()
+
+design_home=Design_home("Home",[(130,35),(125,1)],[("Conteggio","CON"),("Catalogo","CATALOGO"),("Storico",("STORICO"))])
 while(1):
     evento,value=design_home.window.Read()
-    if evento == 'Chiudi':
+    storico_eventi=[] #teniamo conto degli eventi della gui
+    if evento == '_CLOSE_':
         break
-    elif evento == "Conteggio":
-        print("L'ESKERE")
-    elif evento == "Invia":
-        print(value['_INPUT_'])
-        design_home.window.FindElement("_INPUT_").Update("")
+    elif evento == "_CON_":
+        print("Conteggio")
+        #azioniamo il design della pagina richiesta
+    elif evento == "_STORICO_":
+        print("Storico")
+    elif evento == "_CATALOGO_":
+        print("Catalogo")
+    elif evento == "_MIN_":
+        print("eskere")
+    design_home.window.FindElement("_INPUTHome_").Update("")
 design_home.window.Close()
 
 
 #conn.write_tables()
 #conn.inserire_marche()
 #conn.eliminare_marche()
-#conn.inserire_sigarette_vendute()
-#conn.inserire_stecche_sigarette()
 #conn.sigarette_vendute()
 #conn.registrazione_dati_giornalieri()
 #conn.drop_today()
+#conn.inserire_stecche_sigarette()
--- design.py
+++ design.py
@@ -1,38 +1,56 @@
 import PySimpleGUI as sg
 class Design_home:
-    def __init__(self):
+    #title_name= nome pagina
+    #size_put tuple o lista con size output e input, size_put[0]=size output , size_put[1] = size input
+    #name_of_bottoni nomi da dare ai bottoni e le key,una lista composta da varie tuple, ognuna di essa rappresenta un bottone.La tuple è formata dal nome e poi dalla key del bottone.
+    def __init__(self,title_name,size_put,name_of_bottoni):
         #colore del background
         self.color_sfondo="#ccffff" #Colore dello sfondo
         self.color_sottosfondo="#e6ffff" #colore dei bottoni e dell'input e output
         self.color_text_general="#80dfff" #colore dei testi in generale
-        self.text_font_title=("Times",30)
-        self.text_font_botton=("Courier",15)
-        #Layout del frame del titolo
-        self.layout_frame=[
-            [sg.Text("HOME",size=(60,2),justification="center", background_color=self.color_sfondo,text_color=self.color_text_general,font=self.text_font_title)],
+        self.text_font_title=("Times",30) #il font del titolo
+        self.text_font_botton=("Courier",15)# il font dei bottoni
+    #copiamo nelle variabili della classe tutte le informazioni ricevute [per info leggere i commenti sopra il de __init__]
+        self.title_name=title_name
+        self.size_put=size_put
+        self.name_of_bottoni=name_of_bottoni
+        
+         #Layout del frame del titolo  edella parte superiore della
+        self.layout_frame_top=[
+            [sg.Text(self.title_name,size=(60,2),key="_TITLE"+self.title_name+"_",justification="center", background_color=self.color_sfondo,text_color=self.color_text_general,font=self.text_font_title)]
         ]
         #Layout parte destra[output + input]
         self.layout_frame_right=[
-            [sg.Output(size=(110,38), background_color=self.color_sottosfondo,text_color=self.color_text_general)],
-            [sg.InputText("",size=(110,0),background_color=self.color_sottosfondo,text_color=self.color_text_general,key="_INPUT_"),sg.Button("Invia",size=(5,1),button_color=(self.color_text_general,self.color_sottosfondo),font=("Courier",10))],
+            #casella output
+            [sg.Output(size=self.size_put[0], background_color=self.color_sottosfondo,text_color=self.color_text_general)],
+            #barra formata da input + bottone d'invio
+            [sg.InputText("",size=self.size_put[1],background_color=self.color_sottosfondo,text_color=self.color_text_general,key="_INPUT"+title_name+"_"),sg.Button("Invia",size=(5,1),button_color=(self.color_text_general,self.color_sottosfondo),font=("Courier",10))],
 
             
         ]
-        #Bottoni vari
-        self.layout_frame_left=[
-            [sg.Button("Conteggio",size=(20,6),button_color=(self.color_text_general,self.color_sottosfondo),font=self.text_font_botton)],
-            [sg.Button("Catalogo",size=(20,6),button_color=(self.color_text_general,self.color_sottosfondo),font=self.text_font_botton)],
-            [sg.Button("Storico",size=(20,6),button_color=(self.color_text_general,self.color_sottosfondo),font=self.text_font_botton)]
+        #layout bottoni di uscita o di rimpicciolimento della scheda
+        self.layout_frame_bottom=[
+            #text serve solamente per far abbassare i due bottoni nel layout, nulla di più.
+            [sg.Text("",size=(60,2),background_color=self.color_sfondo)],
+            [
+            sg.Button("Minimize",button_color=(self.color_text_general,self.color_sfondo),key="_MIN_", auto_size_button=True),
+            sg.Button("Close",button_color=(self.color_text_general,self.color_sfondo),key="_CLOSE_",  auto_size_button=True),
+            ]
         ]
-        #Layout totale della pagina
+        ###################################################################Creazione del layout dei Bottoni vari####################################################################################################
+        self.layout_frame_left=[]
+        #per ogni tuple creiamo un bottone e lo associamo al layout dei bottoni, x[0] indica il nome e x[1] indica la key
+        for x in self.name_of_bottoni:
+            self.layout_frame_left.append([sg.Button(x[0],size=(20,6),button_color=(self.color_text_general,self.color_sottosfondo),key="_"+x[1]+"_",font=self.text_font_botton)])
+        #####################################################################Layout totale della pagina#########################################################################
         self.layout=[
             #Layout frame del titolo
-            [sg.Frame("",self.layout_frame,border_width=0,pad=(100,0),background_color=self.color_sfondo)],
+            [sg.Frame("",self.layout_frame_top,border_width=0,pad=(100,0),background_color=self.color_sfondo)],
           #layout dei bottoni
-            [sg.Frame("",self.layout_frame_left,border_width=0,pad=(30,0),background_color=self.color_sfondo,),
+            [sg.Frame("",self.layout_frame_left,key="_FRAME_BOTTONI"+self.title_name+"_",border_width=0,pad=(30,0),background_color=self.color_sfondo,),
              #Layout della casella principale(output+input)
              sg.Frame("",self.layout_frame_right,border_width=0,background_color=self.color_sfondo,)],
              #Bottone invia per inviare nella casella output, si trova affianco a quella output
-             [sg.Button("Chiudi")],
+             [sg.Frame("",self.layout_frame_bottom,border_width=0,background_color=self.color_sfondo,pad=(0,0))]
         ]
-        self.window=sg.Window("MANAGER OF CIGARETS",size=(1400,800),location=(0,0),no_titlebar="True",background_color=self.color_sfondo).Layout(self.layout)
+        self.window=sg.Window("MANAGER OF CIGARETS",size=(1400,800),location=(0,0),no_titlebar=True,background_color=self.color_sfondo).Layout(self.layout)
