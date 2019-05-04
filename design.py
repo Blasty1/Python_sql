f0b62317454c33ebcbee4f2b9ab6e064f549ea24
Blasty1 <40994591+Blasty1@users.noreply.github.com>
2019-05-04 19:58:33 +0200

    Update design.py

--- design.py
+++ design.py
@@ -1,25 +1,35 @@
 import tkinter as tk
-window=tk.Tk()
-window.geometry("1000x800")
-window.title("Hello Tkinter")
-window.resizable(False,False)
+class design:
+    def __init__(self):
+        #Finestra principale
+        self.window=tk.Tk()
+        self.window.geometry("1360x760") #grandezza schermo
+        self.window.title("Sigarette Bryan") #titolo della finestra
+        self.window.resizable(False,False) #freeziamo la finestra, cosiche l'utente non puo muoverne le misure
+        self.window.bg("red")
+        
+        #Paramenti Bottoni home
+        self.h_button_home=7 #height 
+        self.w_button_home=40 #width
+        self.padx_home=80 #pad dalla tabella ascisse
+        self.pady_home=100#pad dalla tabella ordinata
+        
+        #1 bottone , CONTEGGIO
+        self.bottone1=tk.Button(self.window,text="CONTEGGIO",height=self.h_button_home,width=self.w_button_home)
+        self.bottone1.grid(row=0,column=0,padx=self.padx_home,pady=self.pady_home)
+        
+        #2 bottone, CATALOGO
+        self.bottone2=tk.Button(self.window,text="CATALOGO",height=self.h_button_home,width=self.w_button_home)
+        self.bottone2.grid(row=0,column=1,padx=self.padx_home,pady=self.pady_home)
+        
+        #3 bottone, STORICO
+        self.bottone3=tk.Button(self.window,text="STORICO",height=self.h_button_home,width=self.w_button_home,bg="red")
+        self.bottone3.grid(row=0,column=2,padx=self.padx_home,pady=self.pady_home)
 
-def first_print():
-    text="Hello World"
-    text_output=tk.Label(window,text=text,fg="red",font= ("Helvetica",16))
-    text_output.grid(row=0,column=1)
-def second_print():
-    text="NUOVO MESSAGGIO BROA"
-    text_output=tk.Label(window,text=text,fg="green")
-    text_output.grid(row=1,column=1,padx=100)
-first_button=tk.Button(text="Saluta",command=first_print)
-first_button.grid(row=0,column=0)
+        #4 
+        
+        self.window.mainloop()
 
-second_button=tk.Button(text="Hello",command=second_print)
-second_button.grid(row=1,column=1)
+de=design()
 
-
-
-if __name__ == "__main__":
-    window.mainloop()
    
