5279460ce937da1b8caf6ad61b88e5e2771f1884
Blasty1 <40994591+Blasty1@users.noreply.github.com>
2019-05-04 12:47:49 +0200

    Add files via upload

--- database.py
+++ database.py
@@ -1,61 +1,7 @@
 from SQL import connection  #classe per tutte le funzioni database-programam
-import PySimpleGUI as sg #classe per la grafica
 from design import Design_home#classe per la grafica
-import mysql.connector #serve per effettuare operazioni sul db
-import datetime #serve per ottenere il datetime corrente
 
 conn=connection('uswJJmW75Q','bxBA2sMC7s','uswJJmW75Q','remotemysql.com')
-#Grafica home
-design_home=Design_home("Home",[(130,35),(125,1)],[("Conteggio","CON"),("Catalogo","CATALOGO"),("Storico","STORICO")])
-while(1):
-    #impostiamo all'inizio del ciclo che la pagina corrente è la home
-    pagina_corrente=design_home.title_name
-    #servirà a prendere in input gli evente (cioè ciò che schiaccia l'utente)
-    evento,value=design_home.window.Read()
-    #se l'utente schiaccia esci allora finirà il ciclo e verrà chiusa la pagina
-    if evento == '_CLOSE_':
-        break
-    elif evento == "_CON_":
-        #azioniamo il design della pagina richiesta
-        design_conteggio=Design_home("Conteggio",[(130,35),(125,1)],[("Singolo Pacchetto","SINGOLO"),("Pacchetti Multipli","MULTIPLI")])
-        #mettiamo non visibile la finestra principale del programma
-        design_home.window.Hide()
-        storico_eventi=[]
-        while(1):
-            #nuove variabili che servono per prendere i soliti dati in input
-            evento2,value2=design_conteggio.window.Read()
-            if evento2 == '_CLOSE_':
-                #salviamo la variabile e gli diamo il nome della pagina che abbiamo appena chiusa
-                pagina_corrente=design_conteggio.title_name
-                design_conteggio.window.Close()
-                break
-            elif evento2 == "_SINGOLO_":
-                #se schiacciamo il bottone singolo, allora scomparirà multiplo 
-                design_conteggio.window.FindElement("_MULTIPLI_").Update(visible=False)
-                #aggiorniamo lo storico eventi, utile sopratutto quando andremo a richiamare la funzione
-                storico_eventi.append(evento2)
-                print("INSERISCI ID SIGARETTA: ")
-            #Se l'ultimo click dell'utente è stato sul bottone singolo e se l'utente invia qualcosa allora quest'ultimo verrà salvato
-            elif storico_eventi[-1] == "_SINGOLO_":
-                if conn.inserire_sigarette_vendute(int(value2["_INPUTConteggio_"])) != 0:
-                    print("Sigaretta Registrata")
-                else:
-                    print("Sigaretta non pervenuta nei registri, ritenta.")
-                design_conteggio.window.FindElement("_INPUTConteggio_").Update("")
-                        
-                
-    elif evento == "_STORICO_":
-        print("Eskere")
-    elif evento == "_CATALOGO_":
-        print("Catalogo")
-    #Se l'ultima pagina parte corrisponde alla home, allora s'ignora questo passaggio, nel caso che abbiamo aperto una pagina prima allora diventa di nuovo visibile la home.
-    if pagina_corrente != design_home.title_name:
-        design_home.window.UnHide()
-        
-    design_home.window.FindElement("_INPUTHome_").Update("")
-design_home.window.Close()
-
-
 #conn.write_tables()
 #conn.inserire_marche()
 #conn.eliminare_marche()
--- design.py
+++ design.py
@@ -1,58 +1,25 @@
-import PySimpleGUI as sg
-class Design_home:
-    #title_name= nome pagina
-    #size_put tuple o lista con size output e input, size_put[0]=size output , size_put[1] = size input
-    #name_of_bottoni nomi da dare ai bottoni e le key,una lista composta da varie tuple, ognuna di essa rappresenta un bottone.La tuple è formata dal nome e poi dalla key del bottone.
-    def __init__(self,title_name,size_put=0,name_of_bottoni=0):
-        #colore del background
-        self.color_sfondo="#ccffff" #Colore dello sfondo
-        self.color_sottosfondo="#e6ffff" #colore dei bottoni e dell'input e output
-        self.color_text_general="#80dfff" #colore dei testi in generale
-        self.text_font_title=("Times",30) #il font del titolo
-        self.text_font_botton=("Courier",15)# il font dei bottoni
-    #copiamo nelle variabili della classe tutte le informazioni ricevute [per info leggere i commenti sopra il de __init__]
-        self.title_name=title_name
-        self.size_put=size_put
-        self.name_of_bottoni=name_of_bottoni
-        
-         #Layout del frame del titolo  edella parte superiore della
-        self.layout_frame_top=[
-            [sg.Text(self.title_name,size=(60,2),key="_TITLE"+self.title_name+"_",justification="center", background_color=self.color_sfondo,text_color=self.color_text_general,font=self.text_font_title)]
-        ]
-        #Layout parte destra[output + input]
-        self.layout_frame_right=[
-            #casella output
-            [sg.Output(size=self.size_put[0], background_color=self.color_sottosfondo,text_color=self.color_text_general)],
-            #barra formata da input + bottone d'invio
-            [sg.InputText("",size=self.size_put[1],background_color=self.color_sottosfondo,text_color=self.color_text_general,key="_INPUT"+self.title_name+"_"),sg.Button("Invia",size=(5,1),button_color=(self.color_text_general,self.color_sottosfondo),font=("Courier",10),key="_INVIO"+self.title_name+"_")],
+import tkinter as tk
+window=tk.Tk()
+window.geometry("1000x800")
+window.title("Hello Tkinter")
+window.resizable(False,False)
 
-            
-        ]
-        #layout bottoni di uscita o di rimpicciolimento della scheda
-        self.layout_frame_bottom=[
-            #text serve solamente per far abbassare i due bottoni nel layout, nulla di più.
-            [sg.Text("",size=(60,2),background_color=self.color_sfondo)],
-            [
-            sg.Button("Minimize",button_color=(self.color_text_general,self.color_sfondo),key="_MIN_", auto_size_button=True,visible=False),
-            sg.Button("Close",button_color=(self.color_text_general,self.color_sfondo),key="_CLOSE_",  auto_size_button=True),
-            ]
-        ]
-        ###################################################################Creazione del layout dei Bottoni vari####################################################################################################
-        self.layout_frame_left=[]
-        #per ogni tuple creiamo un bottone e lo associamo al layout dei bottoni, x[0] indica il nome e x[1] indica la key
-        for x in self.name_of_bottoni:
-            self.layout_frame_left.append([sg.Button(x[0],size=(20,6),button_color=(self.color_text_general,self.color_sottosfondo),key="_"+x[1]+"_",font=self.text_font_botton)])
-        #####################################################################Layout totale della pagina#########################################################################
-        self.layout=[
-            #Layout frame del titolo
-            [sg.Frame("",self.layout_frame_top,border_width=0,pad=(100,0),background_color=self.color_sfondo)],
-          #layout dei bottoni
-            [sg.Frame("",self.layout_frame_left,key="_FRAME_BOTTONI"+self.title_name+"_",border_width=0,pad=(30,0),background_color=self.color_sfondo,),
-             #Layout della casella principale(output+input)
-             sg.Frame("",self.layout_frame_right,border_width=0,background_color=self.color_sfondo,)],
-             #Bottone invia per inviare nella casella output, si trova affianco a quella output
-             [sg.Frame("",self.layout_frame_bottom,border_width=0,background_color=self.color_sfondo,pad=(0,0))]
-            
-        ]
-        self.window=sg.Window("MANAGER OF CIGARETS",size=(1400,800),location=(0,0),background_color=self.color_sfondo,return_keyboard_events=False).Layout(self.layout)
+def first_print():
+    text="Hello World"
+    text_output=tk.Label(window,text=text,fg="red",font= ("Helvetica",16))
+    text_output.grid(row=0,column=1)
+def second_print():
+    text="NUOVO MESSAGGIO BROA"
+    text_output=tk.Label(window,text=text,fg="green")
+    text_output.grid(row=1,column=1,padx=100)
+first_button=tk.Button(text="Saluta",command=first_print)
+first_button.grid(row=0,column=0)
+
+second_button=tk.Button(text="Hello",command=second_print)
+second_button.grid(row=1,column=1)
+
+
+
+if __name__ == "__main__":
+    window.mainloop()
    
