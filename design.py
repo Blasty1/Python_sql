699d1945ae8b6a08c48ebe30524901acde39ae19
Blasty1 <40994591+Blasty1@users.noreply.github.com>
2019-05-05 12:41:35 +0200

    Update design.py

--- design.py
+++ design.py
@@ -18,19 +18,23 @@ class design:
         self.padx_home=80 #pad dalla tabella ascisse
         self.pady_home=100#pad dalla tabella ordinata
         
+        self.titolo=tk.Label(self.window,text="HOME",fg=self.color_font,font=("Helvetica",25),bg=self.sfondo)
+        self.titolo.grid(row=0,column=1)
+        
         #1 bottone , CONTEGGIO
-        self.bottone1=tk.Button(self.window,text="CONTEGGIO",height=self.h_button_home,width=self.w_button_home,bg=self.sfondo,relief="ridge",bd=self.spess_bordi)
-        self.bottone1.grid(row=0,column=0,padx=self.padx_home,pady=self.pady_home)
+        self.bottone1=tk.Button(self.window,text="CONTEGGIO",height=self.h_button_home,width=self.w_button_home,bg=self.sfondo,relief="ridge",bd=self.spess_bordi,fg=self.color_font)
+        self.bottone1.grid(row=1,column=0,padx=self.padx_home,pady=self.pady_home)
         
         #2 bottone, CATALOGO
-        self.bottone2=tk.Button(self.window,text="CATALOGO",height=self.h_button_home,width=self.w_button_home,bg=self.sfondo,relief="groove",bd=self.spess_bordi)
-        self.bottone2.grid(row=0,column=1,padx=self.padx_home,pady=self.pady_home)
+        self.bottone2=tk.Button(self.window,text="CATALOGO",height=self.h_button_home,width=self.w_button_home,bg=self.sfondo,relief="groove",bd=self.spess_bordi,fg=self.color_font)
+        self.bottone2.grid(row=1,column=1,padx=self.padx_home,pady=self.pady_home)
         
         #3 bottone, STORICO
-        self.bottone3=tk.Button(self.window,text="STORICO",height=self.h_button_home,width=self.w_button_home,bg=self.sfondo,relief="ridge",bd=self.spess_bordi)
-        self.bottone3.grid(row=0,column=2,padx=self.padx_home,pady=self.pady_home)
+        self.bottone3=tk.Button(self.window,text="STORICO",height=self.h_button_home,width=self.w_button_home,bg=self.sfondo,relief="ridge",bd=self.spess_bordi,fg=self.color_font)
+        self.bottone3.grid(row=1,column=2,padx=self.padx_home,pady=self.pady_home)
 
-        #4 
+        #4 Barra 
+        
         
         self.window.mainloop()
 
