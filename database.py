4e2a69f0cae6270dea73fda1bc947c171a1cffb3
Blasty1 <40994591+Blasty1@users.noreply.github.com>
2019-05-06 18:34:25 +0200

    Eskere
    mm

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
 from SQL import connection  #classe per tutte le funzioni database-programam
-from design import Design_home#classe per la grafica
+from design import *#classe per la grafica
 
 conn=connection('uswJJmW75Q','bxBA2sMC7s','uswJJmW75Q','remotemysql.com')
+window_principale=design_home(4,30,60,30,["Conteggio","Storico","Catalogo","Home"])
+
 #conn.write_tables()
 #conn.inserire_marche()
 #conn.eliminare_marche()
--- design.py
+++ design.py
@@ -1,10 +1,12 @@
 import tkinter as tk
-class design:
-    def __init__(self):
+class design_home:
+    #titolo[-1] titolo della pagina 
+    #titolo[0] in poi i vari titoli di bottoni 
+    def __init__(self,h_button,w_button,padx,pady,titolo):
         #Parametri grafica
         self.sfondo="#ff8c69"
         self.color_font="#FFFAFA"
-        self.spess_bordi=10
+        self.spess_bordi=8
         #Finestra principale
         self.window=tk.Tk()
         self.window.geometry("1360x760") #grandezza schermo
@@ -13,31 +15,22 @@ class design:
         self.window.configure(bg=self.sfondo)
         
         #Paramenti Bottoni home
-        self.h_button_home=7 #height 
-        self.w_button_home=40 #width
-        self.padx_home=80 #pad dalla tabella ascisse
-        self.pady_home=100#pad dalla tabella ordinata
+        self.h_button_home=h_button #height 
+        self.w_button_home=w_button #width
+        self.padx_home=padx #pad dalla tabella ascisse
+        self.pady_home=pady#pad dalla tabella ordinata
+        self.font=("Times",14)
         
-        self.titolo=tk.Label(self.window,text="HOME",fg=self.color_font,font=("Helvetica",25),bg=self.sfondo)
+        self.titolo=tk.Label(self.window,text=titolo[-1],fg=self.color_font,font=("Courier",40),bg=self.sfondo)
         self.titolo.grid(row=0,column=1)
         
-        #1 bottone , CONTEGGIO
-        self.bottone1=tk.Button(self.window,text="CONTEGGIO",height=self.h_button_home,width=self.w_button_home,bg=self.sfondo,relief="ridge",bd=self.spess_bordi,fg=self.color_font)
-        self.bottone1.grid(row=1,column=0,padx=self.padx_home,pady=self.pady_home)
-        
-        #2 bottone, CATALOGO
-        self.bottone2=tk.Button(self.window,text="CATALOGO",height=self.h_button_home,width=self.w_button_home,bg=self.sfondo,relief="groove",bd=self.spess_bordi,fg=self.color_font)
-        self.bottone2.grid(row=1,column=1,padx=self.padx_home,pady=self.pady_home)
-        
-        #3 bottone, STORICO
-        self.bottone3=tk.Button(self.window,text="STORICO",height=self.h_button_home,width=self.w_button_home,bg=self.sfondo,relief="ridge",bd=self.spess_bordi,fg=self.color_font)
-        self.bottone3.grid(row=1,column=2,padx=self.padx_home,pady=self.pady_home)
-
-        #4 Barra 
+        #Lista di bottoni vari
+        self.bottoni=[]
+        for x in range(len(titolo)-1):                        self.bottoni[x]=tk.Button(self.window,text=titolo[x],height=self.h_button_home,width=self.w_button_home,bg=self.sfondo,relief="ridge",bd=self.spess_bordi,fg=self.color_font,font=self.font)
+            self.bottoni[x].grid(row=1,column=0,padx=self.padx_home,pady=self.pady_home)
         
         
         self.window.mainloop()
 
-de=design()
 
    
