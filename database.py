486a36a565a7f37dea42bd8b98a1fcb28db02939
Blasty1 <40994591+Blasty1@users.noreply.github.com>
2019-05-08 18:40:58 +0200

    Update database.py

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ import design#classe per la grafica
 conn=SQL.connection('uswJJmW75Q','bxBA2sMC7s','uswJJmW75Q','remotemysql.com')
 #creazione finestra principale
 window_principale=design.design_home(2,20,65,70,["Conteggio","Storico","Catalogo","Home"],1)
+window_principale.aggiunta_spazio(["Cia Mi Chiamo Bruno, Come stai? Io bene anche se la vita fa un po schifo , oh mio dio sto a 86 caratteri mo 100."],45)
 #conn.write_tables()
 #conn.inserire_marche()
 #conn.eliminare_marche()
