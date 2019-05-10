98add397e67c49153878a0297d0b0035d369441c
Blasty1 <40994591+Blasty1@users.noreply.github.com>
2019-05-10 17:43:01 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
 import SQL  #classe per tutte le funzioni database-programam
 import design#classe per la grafica
 #connessione al db
-def test():
-    print("ops")
 conn=SQL.connection('uswJJmW75Q','bxBA2sMC7s','uswJJmW75Q','remotemysql.com')
+conteggio_singolo=lambda:design.input_output((700,400))
+#variabile con funzione lambda(anonima) per i bottoni
+window_conteggio=lambda:design.sotto_finestre(2,20,[20,40],[0,30],["Pacchetto Singolo","Pacchetti multipli","Registrazione Giornaliera"],[conteggio_singolo,""])
+window_storico=lambda:design.sotto_finestre(2,20,[20,15],[0,30],["Giornaliero","Mensile","Storico"],["",""])
+window_catalogo=lambda:design.sotto_finestre(2,20,[20,15],[0,30],0,0)
 #creazione finestra principale
-#window_conteggio=design.design_home(2,20,65,70,["Pacchetto Singolo","Pacchetti multipli","Registrazione Giornaliera"],0,["",""])
-window_principale=design.design_home(2,20,65,70,["Conteggio","Storico","Catalogo","Home"],1,[,"",""])
+window_principale=design.design_home(2,20,65,70,[0,0],["Conteggio","Storico","Catalogo","Home"],[window_conteggio,window_storico,window_catalogo])
 
 
 #conn.write_tables()
