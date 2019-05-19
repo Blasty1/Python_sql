c8dd7c046834a325672fe2229668578b3847ca26
Blasty1 <40994591+Blasty1@users.noreply.github.com>
2019-05-19 12:47:58 +0200

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import SQL  #classe per tutte le funzioni database-programam
 import design#classe per la grafica
 #connessione al db
 conn=SQL.connection('uswJJmW75Q','bxBA2sMC7s','uswJJmW75Q','remotemysql.com')
-conteggio_singolo=lambda:design.input_output((700,400))
+conteggio_singolo=lambda:design.input_output((700,400),((70,32),7))
 #variabile con funzione lambda(anonima) per i bottoni
 window_conteggio=lambda:design.sotto_finestre(2,20,[20,40],[0,30],["Pacchetto Singolo","Pacchetti multipli","Registrazione Giornaliera"],[conteggio_singolo,""])
 window_storico=lambda:design.sotto_finestre(2,20,[20,15],[0,30],["Giornaliero","Mensile","Storico"],["",""])
