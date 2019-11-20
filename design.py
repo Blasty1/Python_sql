dbe88b7a73c26b60fd5a72d72fca2d995db24826
BryanTheProgram <42870970+BryanTheProgram@users.noreply.github.com>
2019-11-20 10:09:07 +0100

    Update design.py

--- design.py
+++ design.py
@@ -1,8 +1,18 @@
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
-
-window = Gtk.Window(title="Hello World")
-window.show()
+class home(Gtk.Window):
+    
+    def __init__(self):
+        Gtk.Window.__init__(self,title="Home")
+        self.button=Gtk.Button(label="cliccami")
+        self.button.connect("clicked",self.cliccato)
+        self.add(self.button)
+        
+    def cliccato(self,widget):
+        widget.set_property("label","ciao") #cambiamo la proprietà
+        widget.get_property("label") #ti da la proprietà di un certo widget
+window = home()
 window.connect("destroy", Gtk.main_quit)
+window.show_all()
 Gtk.main()
